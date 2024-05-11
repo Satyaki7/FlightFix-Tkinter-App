dc41d91fc0c19e38fe628c3a46de65d6c45288d4
Satyaki Das <70898206+Satyaki7@users.noreply.github.com>
2024-05-11 10:35:13 +0000

    Changed everything lol

--- main.py
+++ main.py
@@ -3,45 +3,64 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from PIL import Image, ImageTk
 
+def on_entry_click(event):
+   if username.get() == "Username":
+      username.delete(0, tk.END)
+      username.configure(foreground="black")
+
+def on_focus_out(event):
+   if username.get() == "":
+    username.insert(0, "Username")
+    username.configure(foreground="gray")
+
+def on_entry_click1(event):
+   if password.get() == "Password":
+      password.delete(0, tk.END)
+      password.configure(foreground="black")
+
+def on_focus_out1(event):
+   if password.get() == "":
+      password.insert(0, "Password")
+      password.configure(foreground="gray")
+
 #window 
 window = tk.CTk()
 window.title("Testing")
-window.geometry('600x400')
-window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=1)
+window.geometry('900x600')
 
-
-# Left Frame
-left_frame = tk.CTkFrame(window,width=300, height=400)
-left_frame.pack(side="left")
-left_frame.pack_propagate(False)
-#image
-
-logimg = Image.open('loginop.jpg')
-image = ImageTk.PhotoImage(logimg.resize((300,400)))
-label = ttk.Label(left_frame, image=image)
+logimg = Image.open('backoppp.png')
+image = ImageTk.PhotoImage(logimg.resize((900,600)))
+label = ttk.Label(window, image=image)
 label.pack()
 
-# Right Frame
-right_frame = tk.CTkFrame(window, fg_color="white", width=300, height=400)
-right_frame.pack(side="right")
+# Main frame
+frame_width = 300
+frame_height = 300
+frame = tk.CTkFrame(window, width=frame_width, height=frame_height,fg_color = "#F6F7F2",corner_radius=10,border_width=2,border_color="black")
 
-# Centered Frame inside Right Frame
-centered_frame = tk.CTkFrame(right_frame, fg_color="white",width = 250,height = 250,border_width = 1,border_color = "black",)
-centered_frame.place(relx=0.5, rely=0.5, anchor="center")
+frame.place(relx=0.622, rely=0.5, anchor="center")
+frame.pack_propagate (False)
 
 # Entry boxes
-username = ttk.Entry(centered_frame,text = "hello")
-password = ttk.Entry(centered_frame)
-username.pack(padx=10,pady=10)
-password.pack(padx=10,pady=10)
+sign = tk.CTkLabel(frame, text="Sign In", font=("Arial", 24))
+sign.place(relx=0.5, rely=0.3, anchor="center")
+username = ttk.Entry(frame)
+password = ttk.Entry(frame)
+username.insert(0,"Username")
+username.bind("<FocusIn>", on_entry_click)
+username.bind("<FocusOut>", on_focus_out)
+username.place(relx=0.5, rely=0.5, anchor="center")
+password.insert(0,"Password")
+password.bind("<FocusIn>", on_entry_click1)
+password.bind("<FocusOut>", on_focus_out1)
+password.place(relx=0.5, rely=0.65, anchor="center")
 
-#buttons 
-signin = tk.CTkButton(centered_frame, text="Signin",width = 20,fg_color="transparent",hover_color="blue",text_color="black",border_width=1,border_color="black")
-login = tk.CTkButton(centered_frame, text="Login",width = 20,fg_color="transparent",hover_color="blue",text_color="black",border_width=1,border_color="black")
-signin.pack(padx=1, pady=10,side= 'right')
-login.pack(padx=1,pady=10,side= 'right')
+#buttons
+wid = 20
+hei = 10
+signin_button = ttk.Button(frame, text="SignIn",bootstyle="primary")
+signup_button = ttk.Button(frame, text="SignUp",bootstyle="primary-outline")
+signin_button.place(relx=0.4, rely=0.8)
+signup_button.place(relx=0.6, rely=0.8)
 
-
-#run
 window.mainloop()
